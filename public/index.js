bf89c9e6bed65e7ff47bca74349e6bc9f2708c1b
gbull19 <gbull19@yahoo.com>
2022-10-21 14:07:52 -0600

    added drawFive to play again

--- public/index.js
+++ public/index.js
@@ -156,6 +156,7 @@ const reset = () => {
     renderPlayerDuo()
     drawBtn.classList.remove('hide')
     compDuoHeader.classList.add('hide')
+    drawFive() // DRAW
 }
 
 const getPlayerStats = () => {
