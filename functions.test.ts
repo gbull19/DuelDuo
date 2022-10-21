e99e18858bc24540de690d4bd4e33c7ba1bf7416
gbull19 <gbull19@yahoo.com>
2022-10-21 11:11:55 -0600

    ready to deploy

---
+++ functions.test.ts
@@ -0,0 +1,13 @@
+const {shuffleArray} = require('./utils')
+const {bots} = require('./data');
+
+//SUCK SHIT TEST
+
+describe('shuffleArray should randomize the order of input array', () => {
+    test('copy of array has same length', () => 
+        expect(shuffleArray(bots)).toHaveLength(bots.length)
+    );
+    test('copy of array has same contents', () => 
+        expect(shuffleArray(bots)).toContain(bots)
+    );
+})
