f3925a82d9f4a0ae16644507cb0c1fc5c6280a6c
gbull19 <gbull19@yahoo.com>
2022-10-21 12:39:53 -0600

    added html

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ let rollbar = new Rollbar({
   captureUnhandledRejections: true,
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+
 app.get('/styles', (req, res) => {
     try {
         res.sendFile(path.join(__dirname, '../public/index.css'));
